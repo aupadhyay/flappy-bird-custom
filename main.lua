91b9793121f664794861cc0f80aec701ce85f712
Abhi Upadhyay <abhi.upadhyay01@gmail.com>
2015-11-07 13:48:06 -0800

    Added iamges removed Icons

---
+++ main.lua
@@ -0,0 +1,11 @@
+local pipesTop = {}
+local pipesBottom = {}
+
+
+function updateEventListeners(action)
+	if(action == "add")then
+		Runtime:addEventListener
+	elseif(action == "remove")then
+	
+	end
+end
